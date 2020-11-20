c1dbf61b2327ba5778913f6fcb7fad9cc40762cc
Kovacs Sandor <sada93@citromail.hu>
2020-11-20 12:17:05 +0100

    task6

--- app.js
+++ app.js
@@ -5,12 +5,20 @@ var canvasWidth = canvas.width;
 var canvasHeight = canvas.height;
 
 var size = 100;
+
+context.fillStyle = "rgba(255,0,0,.5)";
+context.fillRect( (canvasWidth/2) - (size/4) - (size/2) , (canvasHeight/2) - (size/4) - (size/2) , size , size );
+
+context.fillStyle = "rgba(0,0,255,.5";
+context.fillRect( (canvasWidth/2) - (size/4) , (canvasHeight/2) - (size/4) , size , size );
+
+/*
 var padding = 10;
 
 context.fillStyle = 'green';
 context.fillRect(canvasWidth-size-padding,canvasHeight-size-padding,size,size);
 
-/*context.fillStyle = 'orange';
+context.fillStyle = 'orange';
 context.fillRect(125,50,200,200);
 
 context.beginPath();
